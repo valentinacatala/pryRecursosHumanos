2aebc9cb76209e3104a25965c6d5047a4e0a6a65
Alumno <Alumno@PC6>
2024-05-31 12:58:20 -0300

    Agrego clsConexion

---
+++ pryRecursosHumanos/clsConexionBaseDatos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryRecursosHumanos
+{
+    public class clsConexionBaseDatos
+    {
+        OleDbCommand comando;
+        OleDbConnection conexion;
+        public clsConexionBaseDatos()
+        {
+                
+        }
+    }
+}
